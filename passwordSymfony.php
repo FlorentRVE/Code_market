d6822d90300b0036393f262cdf140cf178d7cc42
FlorentRVE <108260642+FlorentRVE@users.noreply.github.com>
2024-03-05 14:09:16 +0400

    Update passwordSymfony.php

--- passwordSymfony.php
+++ passwordSymfony.php
@@ -81,46 +81,72 @@ public function editPassword(Request $request, User $user, EntityManagerInterfac
 
 /////////// Exemple gestion mot de passe double avec toggle TWIG //////////
 
-$builder->add('plainPassword', RepeatedType::class, [
-    'type' => PasswordType::class,
-    'mapped' => false,
-    'invalid_message' => 'Les mots de passe ne sont pas identiques',
-    'options' => ['attr' => ['class' => 'password-field']],
-    'required' => true,
-    'first_options' => [
-        'label' => 'Mot de passe',
-        'toggle' => true,
-        'hidden_label' => 'Masquer',
-        'visible_label' => 'Afficher',
-        'use_toggle_form_theme' => false,
-        'button_classes' => ['text-slate-50', 'flex', 'justify-end', 'items-center', 'mt-3'],
-        'constraints' => [
-            new NotBlank([
-                'message' => 'Entrez un mot de passe',
-            ]),
-            new Length([
-                'min' => 6,
-                'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
-                'max' => 4096,
-            ]),
-        ],
-    ],
-    'second_options' => [
-        'label' => 'Confirmer le mot de passe',
-        'toggle' => true,
-        'hidden_label' => 'Masquer',
-        'visible_label' => 'Afficher',
-        'use_toggle_form_theme' => false,
-        'button_classes' => ['text-slate-50', 'flex', 'justify-end', 'items-center', 'mt-3'],
-        'constraints' => [
-            new NotBlank([
-                'message' => 'Entrez un mot de passe',
-            ]),
-            new Length([
-                'min' => 6,
-                'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
-                'max' => 4096,
-            ]),
-        ],
-    ],
-]);
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class MotDePasseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'mapped' => false,
+            'invalid_message' => 'Les mots de passe ne sont pas identiques',
+            'options' => ['attr' => ['class' => 'password-field']],
+            'required' => true,
+            'first_options' => [
+                'label' => 'Mot de passe',
+                'toggle' => true,
+                'hidden_label' => 'Masquer',
+                'visible_label' => 'Afficher',
+                'use_toggle_form_theme' => false,
+                'button_classes' => ['text-slate-50', 'flex', 'justify-end', 'items-center', 'mt-3'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+            ],
+            'second_options' => [
+                'label' => 'Confirmer le mot de passe',
+                'toggle' => true,
+                'hidden_label' => 'Masquer',
+                'visible_label' => 'Afficher',
+                'use_toggle_form_theme' => false,
+                'button_classes' => ['text-slate-50', 'flex', 'justify-end', 'items-center', 'mt-3'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
